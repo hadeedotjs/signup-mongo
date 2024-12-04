efb69c9b8b4c0f9fcc5e6b6a537543fa1fb058a9
Hadii <135013774+hadirasool46@users.noreply.github.com>
2024-12-04 19:01:01 +0530

    wrote to a db - first time

---
+++ mongo.js
@@ -0,0 +1,34 @@
+const mongo = require("mongoose")
+const ex = require("express")
+const app = ex()
+app.use(ex.json())
+
+
+mongo.connect
+("mongodb+srv://hadi-rasool:Hsquare%401444@hadi-database.zo37f.mongodb.net/?retryWrites=true&w=majority&appName=hadi-database")
+
+app.post("signup", async (req,res)=>{
+
+    const user = mongo.model("users", {name: String, username: String, password: String })
+    
+    const name = req.body.name
+    const username = req.body.username
+    const password = req.body.password
+
+   
+    const existUser = await user.findOne({username: username})
+    if(existUser){
+        res.status(400).send("user already exists")
+    }
+    const user1 = new user({name: name, username: username, password: password})
+    // user.create({name, username, password})
+    user1.save()
+
+    res.json({
+        msg: "user created successfully"
+    })
+})
+
+
+app.listen(3000)
+
